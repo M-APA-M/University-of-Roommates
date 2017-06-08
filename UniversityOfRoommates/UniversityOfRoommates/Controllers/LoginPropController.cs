8026247454a104f4c60a11e616c925c99251fca6
Michele Mazzamuto <michele.mazzamuto95@yahoo.com>
2017-06-08 13:11:23 +0200

    ok

---
+++ UniversityOfRoommates/UniversityOfRoommates/Controllers/LoginPropController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UniversityOfRoommates.Controllers
+{
+    public class LoginPropController : Controller
+    {
+        // GET: LoginProp
+        public ActionResult LoginProp()
+        {
+            return View();
+        }
+    }
+}
