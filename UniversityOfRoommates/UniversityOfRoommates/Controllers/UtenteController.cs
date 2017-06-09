1c250065ccdb1799781f3ec270a54a9c17acde7b
Michele Mazzamuto <michele.mazzamuto95@yahoo.com>
2017-06-09 15:38:55 +0200

    login-gestisci accout ecc.. fatto su asp.net

--- UniversityOfRoommates/UniversityOfRoommates/Controllers/UtenteController.cs
+++ UniversityOfRoommates/UniversityOfRoommates/Controllers/UtenteController.cs
@@ -8,6 +8,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using UniversityOfRoommates.Models;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 
 namespace UniversityOfRoommates.Controllers
 {
@@ -49,11 +51,24 @@ namespace UniversityOfRoommates.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "nick,pass,nome,cognome,sesso,ddn,cittàProvenienza,email,cell")] Utente utente)
         {
+            try { 
             if (ModelState.IsValid)
             {
                 db.Utenti.Add(utente);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
+            }}
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Trace.TraceInformation("Property: {0} Error: {1}",
+                                                validationError.PropertyName,
+                                                validationError.ErrorMessage);
+                    }
+                }
             }
 
             return View(utente);
@@ -124,5 +139,13 @@ namespace UniversityOfRoommates.Controllers
             }
             base.Dispose(disposing);
         }
+
+
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
     }
 }
